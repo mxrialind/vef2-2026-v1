783ea56bb8ec5ed4af5b59782e0e0e5dcb2a258e
mxrialind <maria@guru.is>
2026-01-29 16:02:29 +0000

    fix: const answer [6]

--- src/lib/parse.js
+++ src/lib/parse.js
@@ -1,3 +1,48 @@
 export function parseQuestions() {
   return 'test';
 }
+
+/**
+ * 
+ * @param {string} line 
+ * @returns 
+ */
+ export function parseLine(line) {
+  const split = line.split(",");
+  /*
+  1   Almenn kunnátta
+  2   Náttúra og vísindi
+  3   Bókmenntir og listir
+  4   Saga
+  5   Landafræði
+  6   Skemmtun og afþreying
+  7   Íþróttir og tómstundir
+  */
+
+  /*
+  1   Nei   Flokkanúmer
+  2   Já    Undirflokkur ef til staðar
+  3   Nei   Erfiðleikastig: 1: Létt, 2: Meðal, 3: Erfið
+  4   Já    Gæðastig: 1: Slöpp, 2: Góð, 3: Ágæt
+  5   Nei   Spurningin
+  6   Nei   Svarið
+  */
+
+  const categoryNumber = split[0];
+  const subCategory = split[1];
+  const difficulty = split[2];
+  const quality = split[3];
+  const question = split[4];
+  const answer = split[6];
+
+  const q = {
+    categoryNumber,
+    subCategory,
+    difficulty,
+    quality,
+    question,
+    answer,
+  };
+
+  return q;
+}
